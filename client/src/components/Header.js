c0ca501e25a401ac516f12ce760da65863cea8d3
Michael <michael.chen880@gmail.com>
2018-06-22 18:56:50 -0700

    minute header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component{
 			<nav>
 				<div className = "nav-wrapper">
 
-					<a className = "left brand-logo">LOGO</a>
+					<a className = "left brand-logo" style = {{"marginLeft":"15px"}}>LOGO</a>
 
 					<ul className = "right">
 						{this.renderContent()}
@@ -27,7 +27,6 @@ class Header extends Component{
 		)
 	}
 }
-
 
 //connects state from store to props, components can access state
 function mapStateToProps({ auth }) {
